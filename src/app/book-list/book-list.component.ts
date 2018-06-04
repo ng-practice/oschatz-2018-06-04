cee6bde404ec6af1d02002db5e9491e9d6a05359
Gregor Woiwode <gregor.woiwode@gmail.com>
2018-06-04 15:41:54 +0200

    book: introduce interface book

--- src/app/book-list/book-list.component.ts
+++ src/app/book-list/book-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Book } from '../book';
 
 @Component({
   selector: 'book-list',
@@ -8,7 +9,7 @@ import { Component, OnInit } from '@angular/core';
 export class BookListComponent {
   books = this.getBooks();
 
-  getBooks() {
+  getBooks(): Book[] {
     return [
       {
         title: 'Design Patterns',
