53fab9e5fcb1f6e50a3bb639cd7db82d856b831d
Gregor Woiwode <gregor.woiwode@gmail.com>
2018-06-05 13:53:05 +0200

    rxjs: provide alternative way to process route params

--- src/app/books/book-detail/book-detail.component.ts
+++ src/app/books/book-detail/book-detail.component.ts
@@ -1,8 +1,10 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { BookService } from '../lib/book.service';
-import { Book } from '../contracts/book';
 import { Subscription } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
+
+import { Book } from '../contracts/book';
+import { BookService } from '../lib/book.service';
 
 @Component({
   selector: 'book-detail',
@@ -13,18 +15,24 @@ export class BookDetailComponent implements OnInit, OnDestroy {
   book: Book = {} as any;
   subscription = Subscription.EMPTY;
 
-  constructor(
-    private route: ActivatedRoute,
-    private bookService: BookService
-  ) {}
+  constructor(private route: ActivatedRoute, private books: BookService) {}
 
   ngOnInit() {
+    // Advanced Level
     this.subscription = this.route.params
-      .subscribe((params: { isbn: string }) =>
-        this.bookService
-          .getBook(params.isbn)
-          .subscribe(book => (this.book = book))
-      );
+      .pipe(
+        switchMap((params: { isbn: string }) => this.books.getBook(params.isbn))
+      )
+      .subscribe(book => (this.book = book));
+      
+    // Beginner Level
+    // this.subscription = this.route.params
+    //   .subscribe((params: { isbn: string }) =>
+    //     this.bookService
+    //       .getBook(params.isbn)
+    //       .subscribe(book => (this.book = book))
+    //   );
+
   }
 
   ngOnDestroy(): void {
