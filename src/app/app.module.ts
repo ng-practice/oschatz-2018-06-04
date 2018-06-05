6c4f770f910ba2692c51b22aa29c9ecab9ceb263
Gregor Woiwode <gregor.woiwode@gmail.com>
2018-06-05 11:14:27 +0200

    use HttpClient

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
@@ -12,6 +13,7 @@ import { BooksModule } from './books/books.module';
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
 
     BooksModule,
     PlaygroundModule
